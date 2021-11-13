ea44b7a5555dff885ec02092797258bf82a6a59a
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-11-13 12:40:30 +0100

    Haciendo pruebas

--- Proyecto 1/Tarea_1.py
+++ Proyecto 1/Tarea_1.py
@@ -2,4 +2,9 @@ import pandas as pd
 import numbers as np
 
 def leer_csv (archivo):
-    return pd.read_csv(archivo, sep = ';')
+    df= pd.read_csv(archivo, sep = ';')
+
+    return df
+
+
+leer_csv('conversiones.py')
